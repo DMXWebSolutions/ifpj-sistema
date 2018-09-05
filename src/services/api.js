8ab9b263e94f7f2ac6b770da41e89c11f87e173d
Daniel Oliveira <danieliver2011@gmail.com>
2018-09-05 12:33:49 -0300

    alterando proxy da api

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const token = sessionStorage.getItem('token')
 
 export const api = axios.create({
-    baseURL: 'http://ifpj-api.atualldigital.com.br/',
+    baseURL: 'https://ifpj-api.atualldigital.com.br/',
     headers: {'x-access-token': token, 'Content-Type': 'application/json' }
 })
